14a07d4904d4037c0ec3856111fb51c8f908ebfb
Anupam <anusrivastava910@gmail.com>
2022-02-10 14:28:54 +0530

    Add files

---
+++ src/main/java/com/example/springmvc/MVC_ValidationAnnotations/Calculator.java
@@ -0,0 +1,23 @@
+package com.example.springmvc.MVC_ValidationAnnotations;
+
+import lombok.Data;
+
+import javax.validation.constraints.*;
+
+@Data
+public class Calculator {
+    @Positive
+    private int operand1;
+
+    @Negative
+    private int operand2;
+
+    @PositiveOrZero
+    private int operand3;
+
+    @NegativeOrZero
+    private int operand4;
+
+    @Null
+    private char operator;
+}
---
+++ src/main/java/com/example/springmvc/MVC_ValidationAnnotations/User.java
@@ -0,0 +1,42 @@
+package com.example.springmvc.MVC_ValidationAnnotations;
+
+import lombok.Data;
+
+import javax.validation.constraints.*;
+
+@Data
+public class User {
+
+    @NotEmpty(message = "First name cannot be null and must have size greater than 0")
+    private String firstName;
+
+    @NotNull(message = "Second name must not be null, empty value/space can be considered")
+    private String lastName;
+
+    @NotBlank(message = "Username must not be null and must contain 1 or more characters")
+    private String userName;;
+
+    @AssertTrue
+    private boolean working;
+
+    @Min(value = 18, message = "Age must be greater than 18")
+    @Max(value = 25, message = "Age must be smaller than 25")
+    private int age;
+
+    @Size(min = 10, max = 200,
+            message = "About Me must be between 10 and 200 characters")
+    private String aboutMe;
+
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{6,10}$")
+    private String password;
+
+    @AssertTrue
+    private boolean isWorking;
+
+    @AssertFalse
+    private boolean isStudent;
+
+}
