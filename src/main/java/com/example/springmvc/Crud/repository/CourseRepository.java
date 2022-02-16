5ac084018298a551d1913e87bd1d8439299a003b
Anupam <anusrivastava910@gmail.com>
2022-02-16 17:06:30 +0530

    Add files

--- src/main/java/com/example/springmvc/Crud/repository/CourseRepository.java
+++ src/main/java/com/example/springmvc/Crud/repository/CourseRepository.java
@@ -5,6 +5,6 @@ import com.example.springmvc.Crud.model.Course;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CourseRepository extends <Course, Long> {
+public interface CourseRepository extends JpaRespository<Course, Long> {
 
 }
